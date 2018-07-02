22d9e0b73dfd3876872b5c5a6084a7647172a037
xcwen <xcwenn@qq.com>
2018-07-02 14:25:39 +0800

    init

--- cpp/framework/sql/exec-sql.sh
+++ cpp/framework/sql/exec-sql.sh
@@ -1,6 +1,6 @@
-mysql -uroot -proot@appinside -e "create database db_tars"
-mysql -uroot -proot@appinside -e "create database tars_stat"
-mysql -uroot -proot@appinside -e "create database tars_property"
+mysql -uroot -pta0mee -e "create database db_tars"
+mysql -uroot -pta0mee -e "create database tars_stat"
+mysql -uroot -pta0mee -e "create database tars_property"
 
-mysql -uroot -proot@appinside db_tars < db_tars.sql
+mysql -uroot -pta0mee db_tars < db_tars.sql
 
